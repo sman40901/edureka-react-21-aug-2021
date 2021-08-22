8899793bad15d35324f328a27d4e4bbdc793695f
sman40901 <sujanmanpr@gmail.com>
2021-08-22 00:01:02 -0700

    click event handler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 // import './Greetings'
 import {PageNotFound} from './pages/PageNotFound';
@@ -16,15 +16,35 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 
-function App() {
-  return(
-    <div className="App">
-      <center>
-        <h2>Welcome to React</h2>
-        <button class="btn btn-success">Get Started</button>
-      </center>
-    </div>
-  );
+// function App() {
+//   return(
+//     <div className="App">
+//       <center>
+//         <h2>Welcome to React</h2>
+//         <button class="btn btn-success">Get Started</button>
+//       </center>
+//     </div>
+//   );
   
+// }
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {liked:false};
+  }
+
+  handleClick=(event)=>{
+    this.setState({liked:!this.state.liked});
+  }
+
+  render(){
+    var text = this.state.liked ? 'like' : 'not lilke'
+    return(
+      <p onClick={this.handleClick}>
+        your {text} this. click to toggle
+      </p>
+    );
+  }
 }
 export default App;
