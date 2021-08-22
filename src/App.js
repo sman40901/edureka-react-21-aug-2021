d17e7a2f30c34c121e732d0c370bb2d78dc42b08
sman40901 <sujanmanpr@gmail.com>
2021-08-22 00:11:54 -0700

    props used

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 // import './Greetings'
-import {PageNotFound} from './pages/PageNotFound';
-import {HomePage} from './pages/HomePage'; // after using named export we need to use {}
-import {PeopleListPage} from './pages/PeopleListPage';
+import { PageNotFound } from './pages/PageNotFound';
+import { HomePage } from './pages/HomePage'; // after using named export we need to use {}
+import { PeopleListPage } from './pages/PeopleListPage';
 
-import {CounterButtonPage} from './pages/CounterButtonPage';
+import { CounterButtonPage } from './pages/CounterButtonPage';
 //import {CongratulationsMessage} from './PeopleListPage'
 // import {useState} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -25,7 +25,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 //       </center>
 //     </div>
 //   );
-  
+
 // }
 
 // class App extends Component{
@@ -48,26 +48,43 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 //   }
 // }
 
-class App extends Component{
-    constructor(props){
-      super(props);
-      this.state = {
-        header:"Header from state",
-        content:"Content from state"
-      };
-    }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      header: "Header from state",
+      content: "Content from state"
+    };
+  }
 
-    render(){
-      return(
-        <div>
-          <h1>
-            {this.state.header}
-          </h1>
-          <h1>
-            {this.state.content}
-          </h1>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <Header headerProp={this.state.header}></Header>
+        <Content contentProp={this.state.content}></Content>
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.headerProp}</h1>
+      </div>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.contentProp}</h1>
+      </div>
+    );
+  }
+
 }
 export default App;
